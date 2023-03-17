0a347bbc5477e399d346116da8022b3d57551414
Karl Chen <76863610+karl-if@users.noreply.github.com>
2023-03-17 21:22:20 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'b440b05bc6c6462fb0a97dc8f5e966cf',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Lab',
+  domain: 'https://agilecxf.notion.site/b440b05bc6c6462fb0a97dc8f5e966cf?v=3232f0fb908c44879705985effdaedda',
+  author: 'Karl Chen',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
