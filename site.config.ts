e31efe9d664bf5b412808a950036252e0c4b0c3a
Karl Chen <76863610+karl-if@users.noreply.github.com>
2023-03-17 21:52:32 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -17,9 +17,9 @@ export default siteConfig({
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  // twitter: 'transitive_bs',
+     twitter: '',
      github: 'karl-if',
-  // linkedin: 'fisch2',
+     linkedin: '',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
