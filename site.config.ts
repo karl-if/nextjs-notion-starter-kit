bfe5612850534ddcd67f0c6807b8ff718c1a566a
karl-if <fan403@outlook.com>
2023-04-03 15:30:03 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '279868994b9a4502963023d742ef41c3',
+  rootNotionPageId: 'Karl-Chen-279868994b9a4502963023d742ef41c3',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Lab',
-  domain: 'https://karl-if.notion.site/1-279868994b9a4502963023d742ef41c3',
+  domain: 'https://karl-if.notion.site/Karl-Chen-279868994b9a4502963023d742ef41c3',
   author: 'Karl Chen',
 
   // open graph metadata (optional)
